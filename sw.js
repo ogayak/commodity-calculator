ea56c9baed1a8ac451edf987210b6a043b058eaf
ogayak <105922304+ogayak@users.noreply.github.com>
2025-08-21 17:01:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,19 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('commodity-calc-v1').then(cache => {
-            return cache.addAll([
-                'index.html',
-                'history.html',
-                'app.js',
-                'style.css',
-                'favicon.png',
-                'manifest.json'
-            ]);
-        })
-    );
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll([
+        "./index.html",
+        "./style.css",
+        "./script.js"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
